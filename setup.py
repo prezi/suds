ebc4740f41b7169aabf787bc730e602a1b081827
David Dul <david.dul@prezi.com>
2015-10-30 14:31:31 -0700

    packaging

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 from prezi.setuputils.git import fetch_version
 
 setup(
-    name='suds',
-    description="A lightweight soap-based client for python. This is a fork.",
-    url='https://github.com/prezi/suds/',
+    name='prezi-suds',
+    description="A lightweight soap-based client for python. This is a fork of the SUDS lib.",
+    url='https://github.com/prezi/prezi-suds/',
     version=fetch_version(__file__),
     packages=find_packages(),
     include_package_data=True,
